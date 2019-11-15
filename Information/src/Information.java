0af2e747ce38b403818bb21c003f36e4a94a659f
Han Yujin <yujin6538@naver.com>
2019-11-15 14:18:50 +0900

    master add commit2

--- Information/src/Information.java
+++ Information/src/Information.java
@@ -4,6 +4,7 @@ public class Information {
 	public static void main(String[] args) {
 		System.out.println("Computer SW");
 		System.out.println("Wonkwang Univ.");
+		System.out.println("Han Yujin");
 	
 	}
 
