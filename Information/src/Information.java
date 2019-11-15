9d0f3d8dc03c4d77e563cdd0271ebcca92953372
Han Yujin <yujin6538@naver.com>
2019-11-15 14:19:30 +0900

    hotfix add commit2

--- Information/src/Information.java
+++ Information/src/Information.java
@@ -4,7 +4,7 @@ public class Information {
 	public static void main(String[] args) {
 		System.out.println("Computer SW");
 		System.out.println("Wonkwang Univ.");
-	
+		System.out.println("20183293");
 	}
 
 }
